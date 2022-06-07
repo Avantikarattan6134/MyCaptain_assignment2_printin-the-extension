92ae3db3635845a78674eb2957370cbe20c4f674
Avantika Rattan <107038031+Avantikarattan6134@users.noreply.github.com>
2022-06-07 23:10:45 +0530

    Created to print the extension
    The program print the extension for the filename entered by the user

---
+++ MyCaptainassignment2.py
@@ -0,0 +1,12 @@
+print("Enter the filename")
+str= input()
+f=str.split(".")
+l=len(f)
+print(l)
+a="py"
+b="java"
+for i in f:
+    if(i== (a) ):
+        print("Python")
+    if(i==(b)):
+        print("Java")
